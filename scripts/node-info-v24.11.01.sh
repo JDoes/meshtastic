153566313d54c9a0b7fc4aec26d2a0e13edb281a
billy <billy@NetGear5G.local>
2024-11-28 13:30:03 -0500

    added --nodes command to node-info script

--- scripts/node-info-v24.11.01.sh
+++ scripts/node-info-v24.11.01.sh
@@ -6,5 +6,9 @@ sleep 5
 
 meshtastic --info > info-out_$(date "+%Y-%m-%d_%H-%M-%S").yaml
 
-echo -e "\nYour outputs were saved in the directory: $(pwd)/ , as the files:\n$(ls -tr | tail -2)"
+sleep 5
+
+meshtastic --nodes > nodes-out-table_$(date "+%Y-%m-%d_%H-%M-%S").txt
+
+echo -e "\nYour outputs were saved in the directory: $(pwd)/ , as the files:\n$(ls -tr | tail -3)"
 echo -e "\nFor more info on backing up and restoring configurations, see the opfficial documentation:\n\thttps://meshtastic.org/docs/software/python/cli/\n\n"
