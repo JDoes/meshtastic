e197ad9271f34ef7a8e4092b7694ff25ac7b383b
billy <billy@NetGear5G.local>
2024-11-28 17:32:10 -0500

    added echo lines for debug purposes

--- scripts/first-config-v24.11.02.sh
+++ scripts/first-config-v24.11.02.sh
@@ -18,13 +18,14 @@ echo -e "\nThis is modeled after:\nhttps://meshtastic.org/docs/configuration/\nI
 	echo
 
 	case $Choice in
-		t | T) echo -e "\n\t\tYou chose to enable Bluetooth"; export BTenable=true ;;
-		f | F) echo -e "\n\t\tYou chose to disable Bluetooth"; export BTenable=false ;;
-		q | Q) echo -e "\n\t\t#*#*#* - QUITTING - *#*#*# "; export BTenable=''; break ;;
+		t | T) echo -e "\n\t\tYou chose to enable Bluetooth"; export BTenabled=true ;;
+		f | F) echo -e "\n\t\tYou chose to disable Bluetooth"; export BTenabled=false ;;
+		q | Q) echo -e "\n\t\t#*#*#* - QUITTING - *#*#*# "; break ;;
 		*) echo "ERROR: Please choose T, F, or Q." ;;
 	esac
 
 #-bluetooth.mode
+#-bluetooth.fixed_pin
         echo -e "\nChoose a setting for bluetooth.mode:"
         echo -e "R) RANDOM_PIN\nF) FIXED_PIN - this feature needs work, you'll have to set your own PIN for now, sorry\nN) NO_PIN\nQ) QUIT"
         read -n1 -p ": " Choice
@@ -34,11 +35,24 @@ echo -e "\nThis is modeled after:\nhttps://meshtastic.org/docs/configuration/\nI
                 r | R) echo -e "\n\t\tYou chose to have a random PIN"; export BTmode=RANDOM_PIN ;;
                 f | F) echo -e "\n\t\tYou chose to have a fixed pin (needs work)"; export BTmode=FIXED_PIN ;;
                 n | N) echo -e "\n\t\tYou chose to have no pin."; export BTmode=NO_PIN ;;
-		q | Q) echo -e "\n\t\t#*#*#* - QUITTING - *#*#*# "; export BTmode=''; break ;;
+		q | Q) echo -e "\n\t\t#*#*#* - QUITTING - *#*#*# "; break ;;
                 *) echo "ERROR: Please choose R, F, N, or Q." ;;
         esac
 
-#-bluetooth.fixed_pin
+	if [[ ${BTmode} == "FIXED_PIN" ]]
+	then
+		read -p "Since you chose FIXED_PIN, enter a 6-digit BT PIN: " PIN_VALUE
+		echo -e "BT Enabled will be set to ${BTenabled} and BT mode will be set to ${BTmode}, and the BT pin will be set to ${PIN_VALUE}"
+		#meshtastic --begin-edit --set bluetooth.enabled ${BTenabled} --set bluetooth.mode ${BTmode} --set bluetooth.fixed_pin ${PIN_VALUE}--commit-edit
+	else
+		echo -e "BT Enabled will be set to ${BTenabled} and BT mode will be set to ${BTmode}"
+		#meshtastic --begin-edit --set bluetooth.enabled ${BTenabled} --set bluetooth.mode ${BTmode} --commit-edit
+	fi
+	
+	unset BTmode
+	unset BTenabled
+	unset PIN_VALUE
+
 ###########################
 ### COME BACK TO THIS #####
 ###########################
