24f96ca38922b4f56c90b7ff21782096d07f00fd
billy <billy@NetGear5G.local>
2024-11-29 08:18:20 -0500

    polished up output

--- scripts/extensive-config.sh
+++ scripts/extensive-config.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
+###################################
+#####                         #####
+#####   Vars and Funct'ns     #####
+#####                         #####
+###################################
+
+DIVIDER="~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+
+#Function is not working properly, I am inexperienced in functions.
+
+REBOOT_MSG="\n\n########################\n### Device rebooting ###\n########################\n\n\tWatch the screen or console.\n\tOr just wait until the node starts responding to --get lora.region or some other arbitrary command.\n\n"
+
+
+###################################
+#####                         #####
+#####     Script begins       #####
+#####                         #####
+###################################
+
+
 clear
 
-echo -e "\nThis is modeled after:\nhttps://meshtastic.org/docs/configuration/\nIf there are any uncertainties or conflicts between this script and the official Meshtastic documentation, always defer to the official docs. The main focus of this script is to do the Radi Configuration specifically. However, there are some Module Config settings in here, too.\n\n\t*** RADIO CONFIG docs: https://meshtastic.org/docs/configuration/radio/\n\t*** MODULE CONFIG docs: https://meshtastic.org/docs/configuration/radio/"
+echo -e "\nThis is modeled after:\nhttps://meshtastic.org/docs/configuration/\nIf there are any conflicts between this script and the official Meshtastic documentation, always defer to the official docs. The main focus of this script is to do two basic configurations:\n\t1) LoRa Region\n\t2) Bluetooth Setup\n*** RADIO CONFIG docs: https://meshtastic.org/docs/configuration/radio/\n"
 
-
+#########################
 #-----------------------
 #BLUETOOTH:
 #____
-
-	echo -e "\nBLUETOOTH: https://meshtastic.org/docs/configuration/radio/bluetooth/\n"
+####
+	echo -e "\n${DIVIDER}\n\nBLUETOOTH:\thttps://meshtastic.org/docs/configuration/radio/bluetooth/\n"
 
 #-bluetooth.enabled: true, false
 	echo -e "\nChoose a setting for bluetooth.enabled:"
@@ -33,60 +54,68 @@ echo -e "\nThis is modeled after:\nhttps://meshtastic.org/docs/configuration/\nI
 
         case $Choice in
                 r | R) echo -e "\n\t\tYou chose to have a random PIN"; export BTmode=RANDOM_PIN ;;
-                f | F) echo -e "\n\t\tYou chose to have a fixed pin (needs work)"; export BTmode=FIXED_PIN ;;
-                n | N) echo -e "\n\t\tYou chose to have no pin."; export BTmode=NO_PIN ;;
+                f | F) echo -e "\n\t\tYou chose to have a fixed PIN"; export BTmode=FIXED_PIN ;;
+                n | N) echo -e "\n\t\tYou chose to have no PIN."; export BTmode=NO_PIN ;;
 		q | Q) echo -e "\n\t\t#*#*#* - QUITTING - *#*#*# "; break ;;
                 *) echo "ERROR: Please choose R, F, N, or Q." ;;
         esac
 
 	if [[ ${BTmode} == "FIXED_PIN" ]]
 	then
+		echo -e ""
 		read -p "Since you chose FIXED_PIN, enter a 6-digit BT PIN: " PIN_VALUE
-		echo -e "BT Enabled will be set to ${BTenabled} and BT mode will be set to ${BTmode}, and the BT pin will be set to ${PIN_VALUE}"
-		#meshtastic --begin-edit --set bluetooth.enabled ${BTenabled} --set bluetooth.mode ${BTmode} --set bluetooth.fixed_pin ${PIN_VALUE}--commit-edit
+		echo -e "\n\t\tBT Enabled will be set to ${BTenabled}\n\t\tBT Mode will be set to ${BTmode}\n\t\tThe BT PIN will be set to ${PIN_VALUE}\n\n"
+		meshtastic --begin-edit --set bluetooth.enabled ${BTenabled} --set bluetooth.mode ${BTmode} --set bluetooth.fixed_pin ${PIN_VALUE} --commit-edit --reboot
 	else
-		echo -e "BT Enabled will be set to ${BTenabled} and BT mode will be set to ${BTmode}"
-		#meshtastic --begin-edit --set bluetooth.enabled ${BTenabled} --set bluetooth.mode ${BTmode} --commit-edit
+		echo -e "\n\t\tBT Enabled will be set to ${BTenabled}\n\t\tBT mode will be set to ${BTmode}\n\n"
+		meshtastic --begin-edit --set bluetooth.enabled ${BTenabled} --set bluetooth.mode ${BTmode} --commit-edit --reboot
 	fi
 	
+
 	unset BTmode
 	unset BTenabled
 	unset PIN_VALUE
 
-
+#########################
 #-----------------------
-#CHANNELS:
 #____
+####
+echo -e "\n${DIVIDER}\n\nCHANNELS:\thttps://meshtastic.org/docs/configuration/radio/channels/"
 
-        echo -e "\nCHANNELS: https://meshtastic.org/docs/configuration/radio/channels/\n"
-	echo -e "\nSee above referenced link to configure channels\n"
-
-
+#########################
 #-----------------------
-#DEVICE:
 #____
+####
+echo -e "DEVICE:\t\thttps://meshtastic.org/docs/configuration/radio/device/"
 
-        echo -e "\nDEVICE: https://meshtastic.org/docs/configuration/radio/device/\n"
-
-
+#########################
 #-----------------------
-#DISPLAY:
 #____
+####
+echo -e "DISPLAY:\thttps://meshtastic.org/docs/configuration/radio/display/"
 
-        echo -e "\nDISPLAY: https://meshtastic.org/docs/configuration/radio/display/\n"
 
+#rebootMessage()
+echo -e ${REBOOT_MSG}
+sleep 10
+
+#########################
 #-----------------------
 #LORA:
 #____
+####
 
-        echo -e "\nLORA: https://meshtastic.org/docs/configuration/radio/lora/"
+	echo -e "\n${DIVIDER}\n\nLORA:\t\thttps://meshtastic.org/docs/configuration/radio/lora/"
 
-#-lora.modem_preset: LONG_FAST, LONG_SLOW, VERY_LONG_SLOW, MEDIUM_SLOW, MEDIUM_FAST, SHORT_SLOW, SHORT_FAST, SHORT_TURBO
 #-lora.use_preset: false, true
 
 #-lora.region: UNSET, US, EU_433, EU_868, CN, JP, ANZ, KR, TW, RU ,IN, NZ_865, TH, LORA_24, UA_433, UA_868, MY_433, MY_919, SG_923
-        read -p "Geographioc regions include: UNSET, US, EU_433, EU_868, CN, JP, ANZ, KR, TW, RU ,IN, NZ_865, TH, LORA_24, UA_433, UA_868, MY_433, MY_919, SG_923. Enter the region: " REGION
-        echo -e "The LoRa region will be set to ${REGION}"
+        echo -e "\nGeographioc regions include: UNSET, US, EU_433, EU_868, CN, JP, ANZ, KR, TW, RU ,IN, NZ_865, TH, LORA_24, UA_433, UA_868, MY_433, MY_919, SG_923" 
+	read -p "Enter the region: " REGION
+        
+	echo -e "\n\t\tThe LoRa region will be set to ${REGION}\n\n"
+	meshtastic --begin-edit --set lora.region ${REGION} --commit-edit --reboot
+
 
 #-lora.bandwidth: 31, 62, 125, 250, 500
 #-lora.spread_factor: 7, 8, 9, 10, 11, 12
@@ -104,55 +133,65 @@ echo -e "\nThis is modeled after:\nhttps://meshtastic.org/docs/configuration/\nI
 
         unset REGION
 
+
+#########################
 #-----------------------
-#NETWORK:
 #____
+####
+echo -e "\n${DIVIDER}\n\nNETWORK:\thttps://meshtastic.org/docs/configuration/radio/network/"
 
-        echo -e "\nNETWORK: https://meshtastic.org/docs/configuration/radio/network/\n"
-
-
+#########################
 #-----------------------
-#POSITION:
 #____
+####
+echo -e "POSITION:\thttps://meshtastic.org/docs/configuration/radio/position/"
 
-        echo -e "\nPOSITION: https://meshtastic.org/docs/configuration/radio/position/\n"
-
-
+#########################
 #-----------------------
-#POWER:
 #____
+####
+echo -e "POWER:\t\thttps://meshtastic.org/docs/configuration/radio/power/"
 
-        echo -e "\nPOWER: https://meshtastic.org/docs/configuration/radio/power/\n"
-
-
+#########################
 #-----------------------
-#SECURITY:
 #____
+####
+echo -e "SECURITY:\thttps://meshtastic.org/docs/configuration/radio/security/"
 
-        echo -e "\nSECURITY: https://meshtastic.org/docs/configuration/radio/security/\n"
-
-
+#########################
 #-----------------------
-#USER:
 #____
+####
+echo -e "USER:\t\thttps://meshtastic.org/docs/configuration/radio/user/"
 
-        echo -e "\nUSER: https://meshtastic.org/docs/configuration/radio/user/\n"
+#rebootMessage()
+echo -e ${REBOOT_MSG}
+sleep 10
+
+###################################
+#####                         #####
+#####   Last Final Review     #####
+#####                         #####
+###################################
+
+echo -e "\n${DIVIDER}\n${DIVIDER}\n\n\t\tSANITY CHECK:\n"
+
+echo -e "Double check the settings below once more:"
+
+echo -e "\n\n$(meshtastic --info | grep -A4 -iw '"bluetooth": {')\n"
+echo -e "\n\n$(meshtastic --info | grep -A18 -w '"lora": {')\n"
+
+echo -e "\n${DIVIDER}\n\nThis concludes the first-time setup.\nFor a more in-depth configuration, always refer to the official docs: https://meshtastic.org/docs/\nAdditionally, here is a helper script to do some of the more advanced configs: /Users/billy/meshtastic-work/github/meshtastic/scripts/extensive-config.sh"
+
+echo -e "\n\n\t\tGoodBye!\n"
 
 
-#-----------------------
-#:
-#____
 
-        echo -e "\n: \n"
-
-
-#-----------------------
-#:
-#____
-
-        echo -e "\n: \n"
-
-
+###################################
+#####                         #####
+#####	Scratch Work Area     #####
+#####                         #####
+###################################
 
 
 #-bluetooth.enabled
@@ -188,4 +227,4 @@ echo -e "\nThis is modeled after:\nhttps://meshtastic.org/docs/configuration/\nI
 
 
 
-echo -e "\nGoodBye!\n"
+
