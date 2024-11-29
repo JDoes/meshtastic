cb3b357460f985675397186c2d627dd99edcc2f7
billy <billy@NetGear5G.local>
2024-11-29 06:06:56 -0500

    uncommented actual meshtastic commands. Script now performs actions on the node.

--- scripts/first-config.sh
+++ scripts/first-config.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+DIVIDER="~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
 clear
 
-echo -e "\nThis is modeled after:\nhttps://meshtastic.org/docs/configuration/\nIf there are any uncertainties or conflicts between this script and the official Meshtastic documentation, always defer to the official docs. The main focus of this script is to do the Radi Configuration specifically. However, there are some Module Config settings in here, too.\n\n\t*** RADIO CONFIG docs: https://meshtastic.org/docs/configuration/radio/\n\t*** MODULE CONFIG docs: https://meshtastic.org/docs/configuration/radio/"
+echo -e "\nThis is modeled after:\nhttps://meshtastic.org/docs/configuration/\nIf there are any conflicts between this script and the official Meshtastic documentation, always defer to the official docs. The main focus of this script is to do two basic configurations:\n\t1) LoRa Region\n\t2) Bluetooth Setup\n*** RADIO CONFIG docs: https://meshtastic.org/docs/configuration/radio/\n"
 
 
 #-----------------------
 #BLUETOOTH:
 #____
 
-	echo -e "\nBLUETOOTH: https://meshtastic.org/docs/configuration/radio/bluetooth/\n"
+	echo -e "\n${DIVIDER}\n\nBLUETOOTH:\thttps://meshtastic.org/docs/configuration/radio/bluetooth/\n"
 
 #-bluetooth.enabled: true, false
 	echo -e "\nChoose a setting for bluetooth.enabled:"
@@ -33,75 +35,73 @@ echo -e "\nThis is modeled after:\nhttps://meshtastic.org/docs/configuration/\nI
 
         case $Choice in
                 r | R) echo -e "\n\t\tYou chose to have a random PIN"; export BTmode=RANDOM_PIN ;;
-                f | F) echo -e "\n\t\tYou chose to have a fixed pin (needs work)"; export BTmode=FIXED_PIN ;;
-                n | N) echo -e "\n\t\tYou chose to have no pin."; export BTmode=NO_PIN ;;
+                f | F) echo -e "\n\t\tYou chose to have a fixed PIN"; export BTmode=FIXED_PIN ;;
+                n | N) echo -e "\n\t\tYou chose to have no PIN."; export BTmode=NO_PIN ;;
 		q | Q) echo -e "\n\t\t#*#*#* - QUITTING - *#*#*# "; break ;;
                 *) echo "ERROR: Please choose R, F, N, or Q." ;;
         esac
 
 	if [[ ${BTmode} == "FIXED_PIN" ]]
 	then
+		echo -e ""
 		read -p "Since you chose FIXED_PIN, enter a 6-digit BT PIN: " PIN_VALUE
-		echo -e "BT Enabled will be set to ${BTenabled} and BT mode will be set to ${BTmode}, and the BT pin will be set to ${PIN_VALUE}"
-		#meshtastic --begin-edit --set bluetooth.enabled ${BTenabled} --set bluetooth.mode ${BTmode} --set bluetooth.fixed_pin ${PIN_VALUE}--commit-edit
+		echo -e "\n\t\tBT Enabled will be set to ${BTenabled}\n\t\tBT Mode will be set to ${BTmode}\n\t\tThe BT PIN will be set to ${PIN_VALUE}"
+		meshtastic --begin-edit --set bluetooth.enabled ${BTenabled} --set bluetooth.mode ${BTmode} --set bluetooth.fixed_pin ${PIN_VALUE}--commit-edit
 	else
-		echo -e "BT Enabled will be set to ${BTenabled} and BT mode will be set to ${BTmode}"
-		#meshtastic --begin-edit --set bluetooth.enabled ${BTenabled} --set bluetooth.mode ${BTmode} --commit-edit
+		echo -e "\n\t\tBT Enabled will be set to ${BTenabled}\n\t\tBT mode will be set to ${BTmode}"
+		meshtastic --begin-edit --set bluetooth.enabled ${BTenabled} --set bluetooth.mode ${BTmode} --commit-edit
 	fi
 	
 	unset BTmode
 	unset BTenabled
 	unset PIN_VALUE
 
+#-----------------------
+echo -e "\n${DIVIDER}\n\nCHANNELS:\thttps://meshtastic.org/docs/configuration/radio/channels/"
 
 #-----------------------
-echo -e "\nCHANNELS: https://meshtastic.org/docs/configuration/radio/channels/\n"
-
-
-#-----------------------
-echo -e "\nDEVICE: https://meshtastic.org/docs/configuration/radio/device/\n"
-
+echo -e "DEVICE:\t\thttps://meshtastic.org/docs/configuration/radio/device/"
 
 #-----------------------
-echo -e "\nDISPLAY: https://meshtastic.org/docs/configuration/radio/display/\n"
-
+echo -e "DISPLAY:\thttps://meshtastic.org/docs/configuration/radio/display/"
 
 #-----------------------
 #LORA:
 #____
 
-	echo -e "\nLORA: https://meshtastic.org/docs/configuration/radio/lora/"
+	echo -e "\n${DIVIDER}\n\nLORA:\t\thttps://meshtastic.org/docs/configuration/radio/lora/"
 
 #-lora.use_preset: false, true
 
 #-lora.region: UNSET, US, EU_433, EU_868, CN, JP, ANZ, KR, TW, RU ,IN, NZ_865, TH, LORA_24, UA_433, UA_868, MY_433, MY_919, SG_923
-        read -p "Geographioc regions include: UNSET, US, EU_433, EU_868, CN, JP, ANZ, KR, TW, RU ,IN, NZ_865, TH, LORA_24, UA_433, UA_868, MY_433, MY_919, SG_923. Enter the region: " REGION
-        echo -e "The LoRa region will be set to ${REGION}"
-	
-	#meshtastic --set lora.region ${REGION}
+        echo -e "\nGeographioc regions include: UNSET, US, EU_433, EU_868, CN, JP, ANZ, KR, TW, RU ,IN, NZ_865, TH, LORA_24, UA_433, UA_868, MY_433, MY_919, SG_923" 
+	read -p "Enter the region: " REGION
+        
+	echo -e "\n\t\tThe LoRa region will be set to ${REGION}"
+	meshtastic --set lora.region ${REGION}
 
 #-lora.tx_enabled: false, true
 
         unset REGION
 
 #-----------------------
-echo -e "\nNETWORK: https://meshtastic.org/docs/configuration/radio/network/\n"
+echo -e "\n${DIVIDER}\n\nNETWORK:\thttps://meshtastic.org/docs/configuration/radio/network/"
 
 
 #-----------------------
-echo -e "\nPOSITION: https://meshtastic.org/docs/configuration/radio/position/\n"
+echo -e "POSITION:\thttps://meshtastic.org/docs/configuration/radio/position/"
 
 
 #-----------------------
-echo -e "\nPOWER: https://meshtastic.org/docs/configuration/radio/power/\n"
+echo -e "POWER:\t\thttps://meshtastic.org/docs/configuration/radio/power/"
 
 
 #-----------------------
-echo -e "\nSECURITY: https://meshtastic.org/docs/configuration/radio/security/\n"
+echo -e "SECURITY:\thttps://meshtastic.org/docs/configuration/radio/security/"
 
 
 #-----------------------
-echo -e "\nUSER: https://meshtastic.org/docs/configuration/radio/user/\n"
+echo -e "USER:\t\thttps://meshtastic.org/docs/configuration/radio/user/"
 
 
-echo -e "\nGoodBye!\n"
+echo -e "\n${DIVIDER}\n\nThis concludes the first-time setup.\n/Users/billy/meshtastic-work/github/meshtastic/scripts/extensive-config.sh\n\n\t\tGoodBye!\n"
